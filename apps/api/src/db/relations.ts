63979d625a4195de878ea0240f33ab3c6d156dbb
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-11 15:12:43 -0300

    feat(api): add relations

--- apps/api/src/db/relations.ts
+++ apps/api/src/db/relations.ts
@@ -2,4 +2,17 @@ import { defineRelations } from 'drizzle-orm';
 
 import * as schema from './schema.ts';
 
-export const relations = defineRelations(schema, (r) => ({}));
+export const relations = defineRelations(schema, (r) => ({
+	posts: {
+		author: r.one.users({
+			from: r.posts.authorId,
+			to: r.users.id,
+		}),
+	},
+	comments: {
+		post: r.one.posts({
+			from: r.comments.postId,
+			to: r.posts.id,
+		}),
+	},
+}));
