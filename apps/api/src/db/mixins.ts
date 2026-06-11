067246ed9b059898fa4e4bedd8ad8a5e189777b0
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-11 15:04:32 -0300

    feat(api): add timetamps mixin

--- apps/api/src/db/mixins.ts
+++ apps/api/src/db/mixins.ts
@@ -1,3 +1,8 @@
-import { integer } from 'drizzle-orm/pg-core';
+import { integer, timestamp } from 'drizzle-orm/pg-core';
 
 export const id = integer().primaryKey().generatedAlwaysAsIdentity();
+
+export const timestamps = {
+	createdAt: timestamp().defaultNow(),
+	updatedAt: timestamp().$onUpdate(() => new Date()),
+};
